398aa20e641202366d09d7f786ba7ebc285ca691
MarwanSobih0 <sobihegypt14@gmail.com>
2023-07-14 18:19:21 -0700

    ss

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the vaule of n status:
+ *		greater than, is zero and is not less than 6.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+		for (m = 49; m <= 57; m++)
+			if (m > n)
+				putchar(n);
+	putchar(m);
+	if (n != 56 || m != 57)
+		putchar(',');
+	putchar(' ');
+	putchar('\n');
+	return (0);
+}
