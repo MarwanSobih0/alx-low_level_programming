a7ef798a76868d3185956ed37e95fc7018abd09d
MarwanSobih0 <sobihegypt14@gmail.com>
2023-07-14 17:31:26 -0700

    ss

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the vaule of n status:
+ *		greater than, is zero and is not less than 6.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
