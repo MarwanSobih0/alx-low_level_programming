26d3feccc9ec111c00af788a5d01d45a0c0a7a9a
MarwanSobih0 <sobihegypt14@gmail.com>
2023-07-14 17:53:53 -0700

    ss

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the vaule of n status:
+ *		greater than, is zero and is not less than 6.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (i = 97; i < 103; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
