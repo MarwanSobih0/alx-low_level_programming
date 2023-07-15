575fb39be250fc89c850637660a700b40e5d446a
MarwanSobih0 <sobihegypt14@gmail.com>
2023-07-14 17:46:02 -0700

    ss

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the vaule of n status:
+ *		greater than, is zero and is not less than 6.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
