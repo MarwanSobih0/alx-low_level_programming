a7c6f47168b5a3410f3e55341753737ad83edc04
MarwanSobih0 <sobihegypt14@gmail.com>
2023-07-14 17:49:06 -0700

    ss

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print the vaule of n status:
+ *		greater than, is zero and is not less than 6.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
