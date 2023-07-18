641d8c87bb2bf4a3c878666f7cd445ce07ef9207
MarwanSobih0 <sobihegypt14@gmail.com>
2023-07-18 05:31:32 -0700

    nn

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,13 @@
 
 int main(void)
 {
-	char c[9] = "_putchar";
-	int i = 0;
-	while (i < 8)
-		putchar(c[i]);
-	i++;
-	putchar('\n');
-	return (0);
+char c[9] = "_putchar";
+int i = 0;
+while (i < 8)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
